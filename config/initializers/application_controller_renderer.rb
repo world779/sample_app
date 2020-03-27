51c61d64709073ce1760d7dae2da1e4b285530e4
world779 <world77914@gmail.com>
2020-03-27 18:26:11 +0900

    add new file

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b3fe3bb82fc1a37682faecc462971de3922892761486ea48c6698a7c4755e23997d09fd016707bb47e0c619c3ea68ceb647f587bf0b118efdec102938f00c71d'
